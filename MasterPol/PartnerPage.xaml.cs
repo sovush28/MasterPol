2ecbd27d38637e1e775807e0c254590969bce5c3
Yulia Ignateva <yuliya.ignateva.06@mail.ru>
2025-09-19 16:58:34 +0500

    Добавьте файлы проекта.

---
+++ MasterPol/PartnerPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MasterPol
+{
+    /// <summary>
+    /// Логика взаимодействия для PartnerPage.xaml
+    /// </summary>
+    public partial class PartnerPage : Page
+    {
+        public PartnerPage()
+        {
+            InitializeComponent();
+            var currentPartners = MasterPolEntities.GetContext().Partner.ToList();
+            PartnerListView.ItemsSource = currentPartners;
+        }
+
+        private void EditBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
